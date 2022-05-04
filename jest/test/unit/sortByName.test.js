08276889244a17060e03aedd99847e2cf0ee7c72
Ufimskii <91329112+Ufimskii@users.noreply.github.com>
2022-05-04 15:10:41 +0500

    vsc_03_task1_add_files

---
+++ jest/test/unit/sortByName.test.js
@@ -0,0 +1,22 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+  it("Books with same titles should not be sorted", () => {
+    expect(
+      sorting.sortByName(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"])
+    ).toEqual(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"]);
+  });
+});
